75dcc839080ab55a96b24b34df68957a60694665
Boyd van Lingen <boydvanlingen@gmail.com>
2022-05-30 15:24:22 +0200

    Begin van components maken voor de enemy
    heb wat values van de enemy controller verandererd en een nieuwe script gemaakt voor het volgen van de player of object.

---
+++ Reusable components/Assets/EnemyFollowing.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowing : MonoBehaviour
+{
+    [SerializeField] GameObject _objectToFollow;
+    private Vector2 _direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public Vector2 moveDir 
+    {
+        get { return _direction; }    
+    
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checked of het links of rechts staat van de enemy
+        float distance = Vector2.Distance(gameObject.transform.position, _objectToFollow.transform.position);
+
+        float otherDistance = _objectToFollow.transform.position.x - gameObject.transform.position.x;
+
+        Debug.Log(otherDistance);
+
+        if (otherDistance > 0)
+            _direction = Vector2.right;
+        else if (otherDistance < 0)
+            _direction = Vector2.left;
+
+    }
+}
